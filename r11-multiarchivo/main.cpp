f429241eb9a90cddf1072a9d8342796f96158609
ValentinDM336 <valentindiazmaya@gmail.com>
2026-09-06 22:40:04 -0600

    R11: programa en tres archivos con Makefile

---
+++ r11-multiarchivo/main.cpp
@@ -0,0 +1,8 @@
+#include "punto.h"
+
+int main() {
+	Punto a;          // Punto()
+	Punto b(5);       // Punto(5)
+	Punto c(1, 2);    // Punto(1,2)
+	return 0;
+}
---
+++ r11-multiarchivo/punto.h
@@ -0,0 +1,10 @@
+#ifndef PUNTO_H
+#define PUNTO_H
+class Punto {
+	int x, y;
+public:
+	Punto();  
+	Punto(int a);
+	Punto(int a, int b);
+};
+#endif
